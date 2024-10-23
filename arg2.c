27c54114fab76c6677e48f0f7cb37fff5eb15cc2
Jack <jackjibb@gmail.com>
2024-10-23 16:33:40 +0100

    default

--- arg2.c
+++ arg2.c
@@ -1,31 +1,37 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
-#include <time.h>
 #include <unistd.h>
-
+#define ARRAY_SIZE 100
 int main(int argc, char **argv) {
   int n;
   int i = 0;
   int sum = 0;
-  int squares[100]; // can this work?
-
-  struct timeval startTime, stopTime;
-  long totalTime; // total time elapsed
-
-  gettimeofday(&startTime, NULL);
+  double time;
   n = atoi(argv[1]);
+  // int squares[100]; // can this work?
+  struct Parallel_Friendly_Array {
+    int array[ARRAY_SIZE];
+  };
+  struct Parallel_Friendly_Array squares;
+  omp_set_dynamic(1);
 
-  for (i = 1; i < n; i++) {
-    squares[i] = i * i;
-    printf("%d %d\n", i, squares[i]);
-    sum += squares[i];
+  omp_get_wtime();
+// Parallel Region
+#pragma omp parallel shared(sum) private(i, squares)
+  {
+#pragma omp for nowait reduction(+ : sum) schedule(dynamic, auto)
+    {
+      for (i = 1; i < n; i++) {
+        squares.array[i] = i * i;
+        // printf("%d %d\n", i, squares.array[i]);
+        sum += squares.array[i];
+      }
+    }
   }
+  time = omp_get_wtime();
   printf("The sum of %d squares is %d\n", n, sum);
 
-  gettimeofday(&stopTime, NULL);
-  totalTime = (stopTime.tv_sec * 1000000 + stopTime.tv_usec) -
-              (startTime.tv_sec * 1000000 + startTime.tv_usec);
-  printf("%ld\n", totalTime);
+  printf("Time Taken: %f\n", time);
   return 0;
 }

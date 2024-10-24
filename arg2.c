ec05cf29b68673eee7c3df52b10bb3d88acbf0b4
Jack <jackjibb@gmail.com>
2024-10-24 13:32:17 +0100

    t

--- arg2.c
+++ arg2.c
@@ -10,11 +10,12 @@ int main(int argc, char **argv) {
   int sum = 0;
   double time;
   n = atoi(argv[1]);
-  struct CacheAlignedInt {
-    int value[16];
-  };
+  // struct CacheAlignedInt {
+  //   int value[16];
+  // };
   // struct CacheAlignedInt array[ARRAY_SIZE];
-  struct CacheAlignedInt *array = malloc(n * sizeof(array->value));
+  // struct CacheAlignedInt *array = malloc(n * sizeof(array->value));
+  int *squares = malloc(n * sizeof(int));
   omp_set_dynamic(1);
 
   time = omp_get_wtime();
@@ -23,15 +24,15 @@ int main(int argc, char **argv) {
   {
 #pragma omp for nowait reduction(+ : sum) schedule(auto)
     for (i = 1; i < n; i++) {
-      array[i].value[0] = i * i;
+      squares[i] = i * i;
       // printf("%d %d\n", i, squares.array[i]);
-      sum += array[i].value[0];
+      sum += squares[i];
     }
   }
   time = omp_get_wtime() - time;
   printf("The sum of %d squares is %d\n", n, sum);
 
   printf("Time Taken: %f\n", time);
-  // free(array);
+  free(squares);
   return 0;
 }
